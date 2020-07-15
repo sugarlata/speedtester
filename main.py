8ad10894e9091919f2035494443556a936cb818d
Nathan Sgarlata <nathan.sgarlata@gmail.com>
2020-07-15 15:45:33 +1000

    Adds correct syntax and things

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import datetime
-import speedtest   
+import speedtest
 
 from ftplib import FTP
 
@@ -9,8 +9,8 @@ FILENAME = 'speeds.csv'
 def main():
     now = datetime.datetime.now()
     now = now.strftime('%Y/%m/%d %H:%M')
-    st = speedtest.Speedtest() 
-    
+    st = speedtest.Speedtest()
+
     st.get_best_server()
 
     dl_speed = st.download()
@@ -19,23 +19,23 @@ def main():
     pg_speed = st.results.ping
 
     with open(FILENAME, 'a+') as f:
-        f.writelines(','.join([
+        f.writelines([','.join([
         now,
-        str(dl_speed),
-        str(ul_speed),
+        str(dl_speed / (1024**2)),
+        str(ul_speed / (1024**2)),
         str(pg_speed)
-        ]))
+        ]), '\n'])
 
     try:
         upload(FILENAME)
-    except:
-        pass
+    except Exception as e:
+        print(e)
 
 
 def upload(filename):
 
-    with FTP(host='192.168.5.215', user=FTP_CREDS['user'], passwd=FTP_CREDS['pass']) as ftp:
-        ftp.login()
+    with FTP(host='192.168.5.215') as ftp:
+        ftp.login(FTP_CREDS['user'], FTP_CREDS['pass'])
         ftp.cwd('/Volumes/SW/speedtest')
 
         f = open(filename, 'rb')
@@ -44,3 +44,5 @@ def upload(filename):
 
 if __name__ == '__main__':
     main()
+
+
