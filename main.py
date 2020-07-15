8e2b3d550b0e33118b84fba57a56dfd00affd7fa
Nathan Sgarlata <nathan.sgarlata@gmail.com>
2020-07-15 14:35:47 +1000

    Adds speed test

---
+++ main.py
@@ -0,0 +1,46 @@
+import datetime
+import speedtest   
+
+from ftplib import FTP
+
+from config import FTP_CREDS
+FILENAME = 'speeds.csv'
+
+def main():
+    now = datetime.datetime.now()
+    now = now.strftime('%Y/%m/%d %H:%M')
+    st = speedtest.Speedtest() 
+    
+    st.get_best_server()
+
+    dl_speed = st.download()
+    ul_speed = st.upload()
+
+    pg_speed = st.results.ping
+
+    with open(FILENAME, 'a+') as f:
+        f.writelines(','.join([
+        now,
+        str(dl_speed),
+        str(ul_speed),
+        str(pg_speed)
+        ]))
+
+    upload(FILENAME)
+
+def upload(filename):
+
+    with FTP(
+        host='192.168.5.215',
+        user=FTP_CREDS['user'],
+        passwd=FTP_CREDS['pass']
+    ) as ftp:
+        ftp.login()
+        ftp.cwd('/Volumes/SW/speedtest')
+
+        f = open(filename, 'rb')
+        ftp.storbinary('STOR %s' % filename, f)
+        f.close()
+
+if __name__ == '__main__':
+    main()
---
+++ run.sh
@@ -0,0 +1,1 @@
+python ~/python/speedtester/main.py
